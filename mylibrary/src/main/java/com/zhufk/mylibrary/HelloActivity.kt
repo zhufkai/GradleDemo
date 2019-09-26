760a911cd3bdccd12971b7006402cf0a12759059
朱福凯 <wb-zhufk@sdc.icbc.com.cn>
2019-09-26 11:35:26 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/zhufk/mylibrary/HelloActivity.kt
@@ -0,0 +1,19 @@
+package com.zhufk.mylibrary
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * @ClassName HelloActivity
+ * @Description TODO
+ * @Author zhufk
+ * @Date 2019/9/26 9:46
+ * @Version 1.0
+ */
+class HelloActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ mylibrary/src/main/java/com/zhufk/mylibrary/HelloWord.kt
@@ -0,0 +1,15 @@
+package com.zhufk.mylibrary
+
+
+/**
+ * @ClassName HelloWord
+ * @Description TODO
+ * @Author zhufk
+ * @Date 2019/9/26 11:09
+ * @Version 1.0
+ */
+class HelloWord {
+    fun say(){
+        System.out.println("Hello World")
+    }
+}
